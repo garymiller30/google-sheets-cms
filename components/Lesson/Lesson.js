8cdbca63d5d1f5302ff28b2e82ad8f9f6f088e90
garymiller30 <garymiller30@gmail.com>
2026-02-10 16:50:11 +0200

    fix

--- components/Lesson/Lesson.js
+++ components/Lesson/Lesson.js
@@ -1,6 +1,6 @@
-import { Box, HStack, Text } from "@chakra-ui/react";
+import { Box, HStack, Link, Text } from "@chakra-ui/react";
 export default function Lesson({ lesson = new Lesson() }) {
-  const { time, teacher, theme, subject, link, timeStart, timeEnd } = lesson;
+  const { teacher, theme, subject, link, timeStart, timeEnd } = lesson;
   return subject ? (
     <Box pt={2} pb={2}>
       <HStack>
@@ -8,20 +8,59 @@ export default function Lesson({ lesson = new Lesson() }) {
           <Text>{timeStart}</Text>
           <Text>{timeEnd}</Text>
         </Box>
-        <Box>
+        <Box minW={0}>
           <HStack>
             <Text fontSize="larger" fontWeight="700" textTransform="uppercase">
               {subject}
             </Text>
             <Text>({teacher})</Text>
           </HStack>
-          <Text as="i">{theme}</Text>
+          {GetLinkComponent(theme)}
+
           <br />
-          <Text wordBreak="break-all" as="code" bg="blue.50">
+          <Text
+            as="code"
+            bg="blue.50"
+            display="block"
+            maxW="100%"
+            whiteSpace="normal"
+            overflowWrap="anywhere"
+            wordBreak="break-all"
+          >
             {link}
           </Text>
         </Box>
       </HStack>
     </Box>
   ) : null;
+}
+
+function GetLinkComponent(theme) {
+  // із рядка теми виділяємо посилання та текст
+  const linkRegex = /https?:\/\/[^\s]+/g;
+  const parts = theme.split(linkRegex).filter((part) => part.trim() !== "");
+  const links = theme.match(linkRegex) || [];
+
+  const components = [];
+
+  for (const link of links) {
+    components.push(LinkComponent(link));
+  }
+
+  for (const part of parts) {
+    components.push(TextConponent(part));
+  }
+  return components;
+}
+
+function LinkComponent(link) {
+  return (
+    <Link href={link} isExternal color="blue.500" fontWeight="500">
+      ZOOM
+    </Link>
+  );
+}
+
+function TextConponent(text) {
+  return <Text>{text}</Text>;
 }
